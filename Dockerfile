f3daca9852f4ea7a624811f7ae795e763c190252
Kostevych Viacheslav <tomkat.s@gmail.com>
2025-07-16 09:23:48 +0300

    Add Deploy

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM bitnami/python:3.9-debian-12
+
+ENV TZ=Europe/Kiev
+
+WORKDIR /app
+
+COPY requirements.txt /app/
+COPY creds/credentials.json /app/creds/credentials.json
+COPY *.py /app/
+
+RUN apt-get update  
+RUN apt-get install  libfbclient2 -y --no-install-recommends
+RUN pip install -r requirements.txt
+
+CMD [ "python3", "main.py" ]
+
+
---
+++ run.sh
@@ -0,0 +1,17 @@
+service=GRobot
+container=$service
+img=tomkat/grobot:2
+
+docker container stop $container
+docker container rm $container 
+
+docker run -dt \
+    -e DB_HOST=192.168.10.5 \
+    -e DB_PATH=cars_dev \
+    -e DB_USER=MONITOR \
+    -e DB_PASSWORD=inwino \
+    -e DELAY=2 \
+    --name=$container \
+    -e TZ=Europe/Kyiv \
+    --restart=always \
+    $img
