a9bc82aef197de1e7b1a26d6e44e32ee40f5f671
Kostevych Viacheslav <tomkat.s@gmail.com>
2025-07-17 18:37:30 +0300

    add url external to env

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ docker container rm $container
 
 docker run -dt \
     -p 8086:3001 \
+    -e URL_TO_CLOUD=http://192.168.10.9:3000/run \
     -e DB_HOST=192.168.10.5 \
     -e DB_PATH=cars_dev \
     -e DB_USER=MONITOR \
